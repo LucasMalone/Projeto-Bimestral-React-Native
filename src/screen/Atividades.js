9c05b0830b5d3d39ced9a3ef3576614aaf23b17e
Guilherme <guilherme.silva2616@etec.sp.gov.br>
2024-06-12 22:14:55 -0300

    atualizacao12.06

--- src/screen/Atividades.js
+++ src/screen/Atividades.js
@@ -1,12 +1,67 @@
-// ReclamacoesSugest.js
-
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Button, Image, TouchableWithoutFeedback, Alert} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Atividades = () => {
+  const [notificacoes, setNotificacoes] = useState([]);
+
+  useEffect(() => {
+    // Adicionar uma notificação ao carregar a página
+    adicionarNotificacao();
+  }, []); // Executa apenas uma vez, quando o componente é montado
+
+  // Função para adicionar uma nova notificação
+  const adicionarNotificacao = () => {
+    const novaNotificacao = {
+      id: Math.random().toString(),
+      usuario: 'Usuario',
+      atividade: 'reagiu ao seu comentário',
+      fotoUsuario: 'https://th.bing.com/th/id/OIP.5yYQS45nqRuMUGZL6lBN9QHaHa?rs=1&pid=ImgDetMain'
+//'https://thumbs.dreamstime.com/b/anonymous-male-profile-sign-circle-man-avatar-graphic-sign-anonymous-male-profile-sign-circle-isolated-white-215427929.jpg'
+    };
+    setNotificacoes([...notificacoes, novaNotificacao]);
+  };
+
+  // Função para excluir uma notificação
+  const excluirNotificacao = (id) => {
+    const novaListaNotificacoes = notificacoes.filter((notificacao) => notificacao.id !== id);
+    setNotificacoes(novaListaNotificacoes);
+  };
+
+  const handleLongPressNotification = (id) => {
+    Alert.alert(
+      [
+      'Opções',
+      'O que você deseja fazer?',
+
+        { text: 'Cancelar', onPress: () => console.log('Cancelar') },
+        { text: 'Excluir', onPress: () => excluirNotificacao(id) },
+        { text: 'Visualizar', onPress: () => alert('Visualizar') }
+      ],
+      { cancelable: true }
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Esta é a página de Atividades</Text>
+     <View style={styles.container}>
+      {/* Renderiza as notificações */}
+      {notificacoes.map((notificacao) => (
+        <TouchableWithoutFeedback key={notificacao.id} onLongPress={() => handleLongPressNotification(notificacao.id)}>
+          <View style={styles.notificacaoContainer}>
+            <View style={styles.notificacaoContent}>
+              <Image source={{ uri: notificacao.fotoUsuario }} style={styles.fotoUsuario} />
+              <Text>{`${notificacao.usuario} ${notificacao.atividade}`}</Text>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      ))}
+
+      {/* Barra de navegação */}
+      <View style={styles.navbarPaginas}>
+        <Icon name="comment" size={30} color="#900" onPress={() => navigation.navigate('ReclamacoesSugest')} />
+        <Icon name="home" size={30} color="#900" onPress={() => navigation.navigate('HomeScreen')} />
+        <Icon name="tasks" size={30} color="#900" onPress={() => navigation.navigate('Atividades')} />
+      </View>
     </View>
   );
 };
@@ -14,10 +69,42 @@ const Atividades = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    paddingHorizontal: 10, // Espaçamento horizontal
+    paddingTop: 10, // Espaçamento superior
+  },
+  notificacaoContainer: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#000',
+    padding: 10,
+    width: '100%'
+  },
+  notificacaoContent: {
+    flexDirection: 'row',
     alignItems: 'center',
+    marginBottom: 10,
+  },
+  fotoUsuario: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  navbarPaginas: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+    paddingVertical: 10,
   },
 });
 
 export default Atividades;
-
--- src/screen/ReclamacoesSugest.js
+++ src/screen/ReclamacoesSugest.js
@@ -1,12 +1,43 @@
 // ReclamacoesSugest.js
-
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const ReclamacoesSugest = () => {
+  const [assunto, setAssunto]= useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  const EnviarReclamacoesouSugestoes=()=>{
+    console.log('Assunto: ', assunto);
+    console.log('Mensagem: ', mensagem);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Esta é a página de Reclamações e Sugestões</Text>
+      <TextInput
+      style={styles.inputAssunto}
+      placeholder="Assunto"
+      value={assunto}
+      onChangeText={text=>setAssunto(text)}
+      />
+
+      <TextInput
+      style={[styles.inputMensagem,{height:150}]}
+      multiline
+      placeholder="Descreva a sua Reclamação ou Sugestão"
+      value={mensagem}
+      onChangeText={text=>setMensagem(text)}
+      />
+
+      <Button
+      title="Enviar" onPress={EnviarReclamacoesouSugestoes}
+      />
+
+      <View style={styles.navbarPaginas}>
+        <Icon name="comment" size={30} color="#900" onPress={() => navigation.navigate('ReclamacoesSugest')} />
+        <Icon name="home" size={30} color="#900" onPress={() => navigation.navigate('HomeScreen')} />
+        <Icon name="tasks" size={30} color="#900" onPress={() => navigation.navigate('Atividades')} />
+      </View>
     </View>
   );
 };
@@ -16,6 +47,35 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+
+  inputAssunto: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 10,
+    width: '100%',
+  },
+
+  inputMensagem:{
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    width: '100%',
+    padding: 10
+  },
+    navbarPaginas: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+    paddingVertical: 10,
   },
 });
 
